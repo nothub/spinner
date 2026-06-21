8a660515a07453ac8f9237a6dc3044f42374cd4d
Florian Hübner <fhuebner@posteo.de>
2026-06-21 21:04:08 +0200

    fmt

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "time"
+	"time"
 
-    "codeberg.org/fhuebner/spinner"
+	"codeberg.org/fhuebner/spinner"
 )
 
 func main() {
-    spinner.Spin("Spinning...", func() { work() },
-        spinner.WithStartDelay(3*time.Second),
-        spinner.WithDelay(300*time.Millisecond),
-    )
+	spinner.Spin("Spinning...", func() { work() },
+		spinner.WithStartDelay(3*time.Second),
+		spinner.WithDelay(300*time.Millisecond),
+	)
 }
 
 func work() {
-    time.Sleep(10 * time.Second)
+	time.Sleep(10 * time.Second)
 }
