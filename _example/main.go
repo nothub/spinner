6a421c5283ff598c201634d376b764d108261b7e
Florian Hübner <fhuebner@posteo.de>
2026-09-04 23:03:16 +0200

    docs: use an atomic counter in the example

--- _example/main.go
+++ _example/main.go
@@ -2,30 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync/atomic"
 	"time"
 
 	"github.com/nothub/spinner"
 )
 
 func main() {
-	spinner.Spin("Working", func() { work() },
+	spinner.Spin("Working", func() { time.Sleep(4 * time.Second) },
 		spinner.WithStartDelay(500*time.Millisecond),
 	)
 
-	c := -1
-	sp := spinner.Start("Spinning 0/20",
+	var done atomic.Int64
+	sp := spinner.Start(fmt.Sprintf("Spinning 0/20"),
 		spinner.WithFrames([]string{"🌑", "🌘", "🌗", "🌖", "🌕", "🌔", "🌓", "🌒"}),
 		spinner.WithStartDelay(0),               // 0 animates instantly, default 3s
 		spinner.WithDelay(200*time.Millisecond), // frame interval, default 250ms
 		spinner.WithLabelFunc(func() string {
-			c = c + 1
-			// regenerates the label every frame
-			return fmt.Sprintf("Spinning %v/20", c)
+			// must be safe for concurrent use
+			return fmt.Sprintf("Spinning %d/20", done.Load())
 		}))
 	defer sp.Stop()
-	work()
-}
-
-func work() {
-	time.Sleep(4 * time.Second)
+	for range 20 {
+		done.Add(1)
+		time.Sleep(200 * time.Millisecond)
+	}
 }
